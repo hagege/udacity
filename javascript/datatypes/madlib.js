216b3b2003056f4d84c6b296daf1493961aacd1f
hagege <webmaster@aachenerkinder.de>
2017-11-19 18:59:28 +0100

    madLib Version 2

--- javascript/datatypes/madlib.js
+++ javascript/datatypes/madlib.js
@@ -8,10 +8,13 @@
  * 'The Intro to JavaScript course is amazing. James and Julia are so fun. I cannot wait to work through the rest of this entertaining content!'
  */
 
-var adjective1 = "amazing";
-var adjective2 = "fun";
-var adjective3 = "entertaining";
-var madLib;
+var adjective1 = 'amazing';
+var adjective2 = 'fun';
+var adjective3 = 'entertaining';
 
-madLib = "The Intro to JavaScript course is " + adjective1 + ". James and Julia are so " + adjective2 + ". I cannot wait to work through the rest of this " + adjective3 + " content!";
+// your code goes here
+var madLib = "The Intro to JavaScript course is " + adjective1 
+            + ". James and Julia are so " + adjective2 
+            + ". I cannot wait to work through the rest of this " + adjective3 + " content!";
+
 console.log(madLib);
