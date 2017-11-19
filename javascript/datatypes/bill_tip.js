25237688fd6674b863d8042e119525a7c0c421d4
hagege <webmaster@aachenerkinder.de>
2017-11-19 18:30:04 +0100

    Änderungen (total nicht gerundet)

--- javascript/datatypes/bill_tip.js
+++ javascript/datatypes/bill_tip.js
@@ -1,4 +1,3 @@
-// JavaScript Document
 /*
  * Programming Quiz: Out to Dinner (2-10)
  */
@@ -7,7 +6,10 @@ var bill;
 var tip;
 var total;
 bill = 10.25 + 3.99 + 7.15;
-tip = bill * 15 / 100;
+tip = bill * 0.15;
 total = bill + tip;
-console.log(total.toFixed(2));
+/* console.log(bill);
+console.log(tip);
+console.log(total.toFixed(2)); */
+console.log(total);
 
