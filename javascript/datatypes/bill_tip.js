edcb5fe72e000b60c4bf3573db8f758166a34a1f
hagege <webmaster@aachenerkinder.de>
2017-11-19 18:23:44 +0100

    Lektion 11, Nummer 22

---
+++ javascript/datatypes/bill_tip.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+/*
+ * Programming Quiz: Out to Dinner (2-10)
+ */
+
+var bill;
+var tip;
+var total;
+bill = 10.25 + 3.99 + 7.15;
+tip = bill * 15 / 100;
+total = bill + tip;
+console.log(total.toFixed(2));
+
