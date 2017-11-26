1b8c4f261aba9875334a28ed4374398f406b9e00
hagege <webmaster@aachenerkinder.de>
2017-11-26 11:18:07 +0100

    kein else - Zwei gewünscht

--- javascript/datatypes/icecream.js
+++ javascript/datatypes/icecream.js
@@ -21,6 +21,4 @@ var toppings = "cookies";
 
 if ((flavor === "vanilla" || flavor === "chocolate") && (vessel === "cone" || vessel === "bowl") && (toppings === "sprinkles" || toppings === "peanuts")) {
     console.log("I'd like two scoops of " + flavor + " ice cream in a " + vessel + " with " + toppings);
-} else {
-    console.log("Not possible!");
 }
