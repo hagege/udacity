9fc31bb854fbb4ad8e89be6ac297a56556734f16
hagege <webmaster@aachenerkinder.de>
2017-11-26 14:43:33 +0100

    andere Lösung

--- javascript/datatypes/wear.js
+++ javascript/datatypes/wear.js
@@ -3,35 +3,28 @@
  */
 
 // change the values of `shirtWidth`, `shirtLength`, and `shirtSleeve` to test your code
-var shirtWidth = 23;
-var shirtLength = 30;
-var shirtSleeve = 8.71;
-var ok = false;
+var shirtWidth = 18;
+var shirtLength = 29;
+var shirtSleeve = 8.47;
 
 if (shirtWidth <= 18 && shirtLength <= 28 && shirtSleeve <= 8.37) {
     console.log ("S");
-    ok = true;
-}
-if ((shirtWidth >= 20 && shirtWidth <= 21) && shirtLength === 29 && (shirtSleeve >= 8.38 && shirtSleeve <= 8.62)) {
-    console.log ("M");
-    ok = true;
-}
-if ((shirtWidth >= 22 && shirtWidth <= 23) && shirtLength === 30 && (shirtSleeve >= 8.63 && shirtSleeve <= 8.87)) {
-    console.log ("L");
-    ok = true;
-}
-if ((shirtWidth >= 24 && shirtWidth <= 25) && (shirtLength >= 31 && shirtLength <= 32) && (shirtSleeve >= 8.88 && shirtSleeve <= 9.62)) {
-    console.log ("XL");
-    ok = true;
-}
-if ((shirtWidth >= 26 && shirtWidth <= 27) && shirtLength === 33 && (shirtSleeve >= 9.63 && shirtSleeve <= 10.12)) {
-    console.log ("2XL");
-    ok = true;    
-}
-if (shirtWidth >= 28 && shirtLength >= 34 && shirtSleeve >= 10.13) {
-    console.log ("3XL");
-    ok = true;    
-}
-if (ok === false) {
+    }
+    else if ((shirtWidth >= 20 && shirtWidth <= 21) && shirtLength === 29 && (shirtSleeve >= 8.38 && shirtSleeve <= 8.62)) {
+        console.log ("M");
+    }
+    else if ((shirtWidth >= 22 && shirtWidth <= 23) && shirtLength === 30 && (shirtSleeve >= 8.63 && shirtSleeve <= 8.87)) {
+        console.log ("L");
+    }
+    else if ((shirtWidth >= 24 && shirtWidth <= 25) && (shirtLength >= 31 && shirtLength <= 32) && (shirtSleeve >= 8.88 && shirtSleeve <= 9.62)) {
+        console.log ("XL");
+    }
+    else if ((shirtWidth >= 26 && shirtWidth <= 27) && shirtLength === 33 && (shirtSleeve >= 9.63 && shirtSleeve <= 10.12)) {
+        console.log ("2XL");
+    }
+    else if (shirtWidth >= 28 && shirtLength >= 34 && shirtSleeve >= 10.13) {
+        console.log ("3XL");
+    } 
+    else {
     console.log ("N/A");
 }
