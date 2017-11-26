641b37f4c33f1f83cb74254a9124643010f61726
hagege <webmaster@aachenerkinder.de>
2017-11-26 14:56:07 +0100

    korrekter Ablauf

--- javascript/datatypes/wear.js
+++ javascript/datatypes/wear.js
@@ -8,23 +8,23 @@ var shirtLength = 30;
 var shirtSleeve = 8.71;
 
 if ((shirtWidth >= 18 && shirtWidth < 20) && (shirtLength >= 28 && shirtLength < 29) && (shirtSleeve >= 8.13 && shirtSleeve < 8.38)) {
-    console.log ("S");
+    console.log("S");
     }
-    else if ((shirtWidth >= 20 && shirtWidth <= 21) && shirtLength === 29 && (shirtSleeve >= 8.38 && shirtSleeve <= 8.62)) {
-        console.log ("M");
+    else if ((shirtWidth >= 20 && shirtWidth < 22) && (shirtLength >= 29 && shirtLength < 30) && (shirtSleeve >= 8.38 && shirtSleeve < 8.63)) {
+    console.log("M");
     }
-    else if ((shirtWidth >= 22 && shirtWidth <= 23) && shirtLength === 30 && (shirtSleeve >= 8.63 && shirtSleeve <= 8.87)) {
-        console.log ("L");
+    else if ((shirtWidth >= 22 && shirtWidth < 24) && (shirtLength >= 30 && shirtLength < 31) && (shirtSleeve >=8.63 && shirtSleeve < 8.88)) {
+    console.log("L");
     }
-    else if ((shirtWidth >= 24 && shirtWidth <= 25) && (shirtLength >= 31 && shirtLength <= 32) && (shirtSleeve >= 8.88 && shirtSleeve <= 9.62)) {
-        console.log ("XL");
+    else if ((shirtWidth >= 24 && shirtWidth < 26) && (shirtLength >= 31 && shirtLength < 33) && (shirtSleeve >= 8.88 && shirtSleeve < 9.63)) {
+    console.log("XL");
     }
-    else if ((shirtWidth >= 26 && shirtWidth <= 27) && shirtLength === 33 && (shirtSleeve >= 9.63 && shirtSleeve <= 10.12)) {
-        console.log ("2XL");
+    else if ((shirtWidth >= 26 && shirtWidth < 28) && (shirtLength >= 33 && shirtLength < 34) && (shirtSleeve >= 9.63 && shirtSleeve < 10.13)) {
+    console.log("2XL");
     }
     else if (shirtWidth >= 28 && shirtLength >= 34 && shirtSleeve >= 10.13) {
-        console.log ("3XL");
-    } 
+    console.log("3XL");
+    }
     else {
-    console.log ("N/A");
+    console.log("N/A");
 }
