0a742dc5f936e5c58d0ff005dece51f02efdb1ec
hagege <webmaster@aachenerkinder.de>
2017-11-27 20:00:22 +0100

    Änderung der Variablen
    Ebenfalls ohne Auswirkung

--- javascript/datatypes/back_to_school.js
+++ javascript/datatypes/back_to_school.js
@@ -3,7 +3,7 @@
  */
 
 // change the value of `education` to test your code
-var education = "an Associate's degree";
+var education = "no high school diploma";
 
 // set the value of this based on a person's education
 var salary;
@@ -11,25 +11,25 @@ var salary;
 
 switch (education) {
   case "no high school diploma":
-    salary = "$25,636/year";
+    salary = 25,636;
     break;
   case "a high school diploma":
-    salary = "$35,256/year";
+    salary = 35,256;
     break;
   case "an Associate's degree":
-    salary = "$41,496/year";
+    salary = 41,496;
     break;
   case "a Bachelor's degree":
-    salary = "$59,124/year";
+    salary = 59,124;
     break;
   case "a Master's degree":
-    salary = "$69,732/year";
+    salary = 69,732;
     break;
   case "a Professional degree":
-    salary = "$89,960/year";
+    salary = 89,960;
     break;
   case "a Doctoral degree":
-    salary = "$84,396/year";
-    break;
+    salary = 84,396;
 }
-console.log("In 2015, a person with " + education + " earned an average of " + salary.toLocaleString("en-US"));
+/* console.log("In 2015, a person with " + education + " earned an average of $" + salary + "/year"); */
+console.log("In 2015, a person with " + education + " earned an average of " + salary + "/year.");
