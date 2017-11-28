10ee8f1553f054e9ec05d1e6c2c44d1b74bad493
hagege <webmaster@aachenerkinder.de>
2017-11-28 19:48:48 +0100

    Fehlende Bedingungen durch switch ergänzt

--- javascript/lesson13/countdown.js
+++ javascript/lesson13/countdown.js
@@ -7,12 +7,25 @@
 var num = 60;
 
 while (num > 0) {
-    if (num != 50){
-        console.log("T-" + num + " seconds"); 
+    switch (num) {
+        case 6:
+            console.log("Main engine start");
+            break;
+        case 10:
+            console.log("Activate main engine hydrogen burnoff system");
+            break;
+        case 16:
+            console.log("Activate launch pad sound suppression system");
+            break;
+        case 31:
+            console.log("Ground launch sequencer is go for auto sequence start");
+            break;
+        case 50:
+            console.log("Orbiter transfers from ground to internal power");
+            break;
+        default:
+            console.log("T-" + num + " seconds"); 
     }
-    else {
-        console.log("Orbiter transfers from ground to internal power"); 
-    }    
     num = num - 1;    
 }
 console.log("Solid rocket booster ignition and liftoff!");
