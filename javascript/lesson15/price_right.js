6767cbc3857e07535a990b73f480cd9366523690
hagege <webmaster@aachenerkinder.de>
2017-12-03 16:36:14 +0100

    Änderung der Preise

---
+++ javascript/lesson15/price_right.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ */
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+prices[0]=2.34;
+prices[2]=1.28;
+prices[6]=5.66;
+console.log(prices);
+
+
