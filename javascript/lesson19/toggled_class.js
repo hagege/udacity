4c07706af4ab7604732ef76b9617ffc01fc3a610
hagege <webmaster@aachenerkinder.de>
2017-12-27 11:42:13 +0100

    lesson_19_5_modifying_css
    Fast ok gewesen

--- javascript/lesson19/toggled_class.js
+++ javascript/lesson19/toggled_class.js
@@ -2,6 +2,8 @@
 For this quiz, use a jQuery class selector and featuredArticle variable to toggle the 'featured' class!
 */
 
+// damit wird die Klasse .featured bei dem Artikel ausgeschaltet.
+
 // don't change this variable!
 var featuredArticle;
 
